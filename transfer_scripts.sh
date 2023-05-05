113cb5600630932b5081e54464cbeb3ec30bd1c1
rro2q2 <rro2q2@mail.missouri.edu>
2023-05-05 01:52:46 +0000

    transfer modified neuralhydrology scripts

---
+++ transfer_scripts.sh
@@ -0,0 +1,3 @@
+cp scripts/camelsus.py neuralhydrology/neuralhydrology/datasetzoo/
+cp scripts/camelscl.py neuralhydrology/neuralhydrology/datasetzoo/
+cp scripts/__init__.py neuralhydrology/neuralhydrology/modelzoo/
